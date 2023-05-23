c7cc5b3158d9eec22abfd047b7bd9d30a1a48873
Luis Angel Caro Galoc <77682546+angelcgdev@users.noreply.github.com>
2023-05-23 06:40:07 -0500

    code fix: API change from Confluend to Jira

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,26 @@
 import api, { route } from '@forge/api';
-import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState, useEffect } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState, useEffect, Button } from '@forge/ui';
 
-const fetchCommentsForContent = async (contentId) => {
-  const res = await api.asUser().requestConfluence(route`/wiki/rest/api/content/${contentId}/child/comment`);
+const fetchCommentsForIssue  = async (issueIdOrKey) => {
+  const res = await api.asUser().requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`);
   const data = await res.json();
-  return data.results;
+  return data.comments;
 };
 const App = () => {
 
-  const context = useProductContext ();
+  const context = useProductContext();
   const [comments, setComments] = useState([]);
 
+  const handleFetchComments = async()=>{
+    const data = await fetchCommentsForIssue(context.platformContext.issueKey)
+    console.log({data});
+    if(data){
+      setComments(data);
+    };
+  };
+
   useEffect(() => {
-    (async()=>{
-      const data = await fetchCommentsForContent(context.contentId)
-      console.log({data});
-      if(data){
-        setComments(data);
-      };
-    })()
+    handleFetchComments();
   }, [])
 
   console.log(`Number of comments on this page: ${comments.length}`);
@@ -28,6 +30,7 @@ const App = () => {
       <Text>
         Number of comments on this page: {comments.length}
       </Text>
+      <Button onClick={handleFetchComments} text='Reload'/>
     </Fragment>
   );
 };
